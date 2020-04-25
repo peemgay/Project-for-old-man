503db4b452747f7f710044798036948fdb29d698
peemgay <peempeem2@gmail.com>
2020-04-25 21:21:15 +0700

    first commit

---
+++ Project for old man/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project for old man
+//
+//  Created by Peem on 25/4/2563 BE.
+//  Copyright © 2563 Peem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
